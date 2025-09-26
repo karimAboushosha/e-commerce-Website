02546f3747ea351dbfe62ab6dd13e7879fe578bc
Karim Aboushosha <karimaboushosha1996@hotmail.com>
2025-09-27 02:43:48 +0300

    Fix middleware cookie check for NextAuth on Vercel

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,13 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
- 
+
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
 
-const token = request.cookies.get("next-auth.session-token")
+const token = 
+
+    request.cookies.get("next-auth.session-token")?.value ||
+    request.cookies.get("__Secure-next-auth.session-token")?.value;
 
     if(!token){
         return NextResponse.redirect(new URL('/login', request.url))
