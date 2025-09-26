b8ac6cd9dde7711e2f26f0bc5abe25b965602cb6
Karim Aboushosha <karimaboushosha1996@hotmail.com>
2025-09-26 21:23:43 +0300

    Fix Products page: use ProductsGrid without props

--- src/app/(routes)/products/page.tsx
+++ src/app/(routes)/products/page.tsx
@@ -2,12 +2,24 @@ import getProducts from '@/app/actions/products.action';
 import ProductsGrid from '@/Components/products-component/productsGrid';
 import React from 'react'
 
-export default async function Products() {
+// export default async function Products() {
 
-  const {data} = await getProducts();
+//   const {data} = await getProducts() as any;
+//   return (
+//     <div>
+//       <div> <ProductsGrid products ={data} /></div>
+//     </div>
+//   )
+// }
+
+export default async function Products() {
+  const { data } = await getProducts() as any;
+
   return (
     <div>
-      <div> <ProductsGrid products ={data} /></div>
+      <div>
+        <ProductsGrid />
+      </div>
     </div>
-  )
+  );
 }
